4a5de1326a8f0930a23776725e6764e0c988933c
cccaternberg <acaternberg@cloudbees.com>
2019-06-12 17:41:37 +0200

    add

---
+++ k8s/gke/README.md
@@ -0,0 +1,3 @@
+#Pre-Requirements: 
+
+see https://go.cloudbees.com/docs/cloudbees-core/cloud-install-guide/gke-install/#prerequisites 
---
+++ k8s/gke/create-cluster.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+
+
+gcloud auth login
+CLUSTER_NAME=$(gcloud config get-value account)
+CLUSTER_NAME=ps-dev-${CLUSTER_NAME%@*}
+echo $CLUSTER_NAME
+
+
+
+NAME=$CLUSTER_NAME && REGION=us-east1 && MACHINE_TYPE=n1-standard-2
+
+MIN_NODES=1 && MAX_NODES=3
+
+gcloud container clusters create $NAME --region $REGION \
+    --machine-type $MACHINE_TYPE --enable-autoscaling \
+    --num-nodes 1 --max-nodes $MAX_NODES --min-nodes $MIN_NODES
+
+kubectl create clusterrolebinding cluster-admin-binding \
+    --clusterrole cluster-admin \
+    --user $(gcloud config get-value account)
