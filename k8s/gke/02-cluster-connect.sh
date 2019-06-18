350283d96f6ac53d1447e738cb09a458b8038364
cccaternberg <acaternberg@cloudbees.com>
2019-06-18 14:58:24 +0200

    use variable for clustername

--- k8s/gke/02-cluster-connect.sh
+++ k8s/gke/02-cluster-connect.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source ./set-env.sh
-gcloud beta container clusters get-credentials ps-dev-acaternberg --region us-east1 --project $PROJECT_ID
+gcloud beta container clusters get-credentials $CLUSTER_NAME --region us-east1 --project $PROJECT_ID
