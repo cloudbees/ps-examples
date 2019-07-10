5f2fc675d17b5307bf3e9c2539959ad8c4c4dd66
cccaternberg <acaternberg@cloudbees.com>
2019-07-10 12:37:17 +0200

    improve

--- k8s/gke/set-env.sh
+++ k8s/gke/set-env.sh
@@ -1,27 +1,40 @@
 #!/usr/bin/env bash
 
-GOOGLE_CREDENTIALS="google-credentials.json"
+export KUBE_CONFIG=~/.kube/config
+export PROJECT_ID=ps-dev-201405
+export PROJECT_RESOURCE=//cloudresourcemanager.googleapis.com/projects/$PROJECT_ID
+export GOOGLE_CREDENTIALS="google-credentials.json"
+export NAMESPACE=cje
 if [ -f $(pwd)/"$GOOGLE_CREDENTIALS" ]
 then
     export GOOGLE_APPLICATION_CREDENTIALS=$(pwd)/$GOOGLE_CREDENTIALS
 else
     gcloud auth login --quiet
-    SA="$(gcloud config get-value account).iam.gserviceaccount.com"
-   # gcloud iam list-grantable-roles
-    gcloud iam service-accounts keys list  --iam-account $SA
-    gcloud iam service-accounts keys create $GOOGLE_CREDENTIALS  --iam-account=$SA
-
 fi
+
 export CLUSTER_NAME=$(gcloud config get-value account)
 export CLUSTER_NAME=ps-dev-${CLUSTER_NAME%@*}
-export KUBE_CONFIG=~/.kube/config
-export PROJECT_ID=ps-dev-201405
+
+
+#set defaults
 gcloud config set project $PROJECT_ID
 #gcloud config set compute/zone us-east1-b,us-east1-d,us-east1-c
 gcloud config set compute/zone us-east1
 gcloud config set compute/region us-east1
 gcloud components update
+
+
+#ccreate creds
+SA="$(gcloud config get-value account).iam.gserviceaccount.com"
+echo "#######list-grantable-roles#######"
+gcloud iam list-grantable-roles $PROJECT_RESOURCE --format json
+echo "#######service-accounts keyss#######"
+gcloud iam service-accounts keys list  --iam-account $SA --format json
+gcloud iam service-accounts keys create $GOOGLE_CREDENTIALS  --iam-account=$SA
+
 echo "CLUSTER_NAME: $CLUSTER_NAME"
-export NAMESPACE=cje
+echo "PROJECT_ID: $PROJECT_ID"
+echo "SERVICE_ACCOUNT (SA): $SA"
+
 
 
